7b0bb531640aa88e4cfcfddcd324bdf9f25f4838
kaiwangsim <believemk@163.com>
2020-06-15 00:25:11 +0800

    Update bs ku.py
    .+

--- bs ku.py
+++ bs ku.py
@@ -4,20 +4,31 @@ from bs4 import BeautifulSoup
 import re
 import requests
 
-url1 = 'https://m.weibo.cn/detail/4515667271909478'
+url1 = 'https://m.weibo.cn/detail/4515657263225384'
 r1 = requests.get(url1)
 soup = BeautifulSoup(r1.text,"html.parser")
 
-pattern = re.compile(r"var $render_data = '(.*?)';$", re.MULTILINE | re.DOTALL)
-script = soup.find_all("script", text=pattern)
 
-s1 = soup.find_all("script")
+script_content = soup.find_all("script")[1]
 
-t = 2
-for i in soup.find_all('script'):
-    a = i
-    t -=1
-    if t == 0:
-        break
-    
-    
+
+aa = re.compile(r'"text".+')
+
+aa1 = re.compile(r'created_at.+')
+
+aa2 = aa1.findall(script_content.text)
+
+a1 = aa.findall(script_content.text)
+
+x = a1[0]
+
+pat=re.compile(r'[\u4e00-\u9fa5]+')
+xx = pat.findall(x)
+
+
+
+jj=''
+for i in xx:
+    jj = jj + i
+
+print(jj)
